53edc183ec954a83f67cd8a0eea6af7ebf6d66c0
Robin Kamo <robin.kamo@codic.se>
2021-10-13 14:45:42 +0200

    infra

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+if __name__ == '__main__':
+  app.run(debug=True, port=5000, host='0.0.0.0')
